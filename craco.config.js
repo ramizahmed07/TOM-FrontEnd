7bd449cf7f63cf4b63b8ba092dd8540472a1a085
Rizwan Ahmed <rizwan.ahmed19@outlook.com>
2021-07-09 18:54:39 +0500

    Setup ant design

---
+++ craco.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const CracoLessPlugin = require("craco-less");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@assets": path.resolve(__dirname, "src/assets/"),
+    },
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: {
+              // fonts
+              "@font-family": "Montserrat, sans-serif",
+              "@secondary-font": "Space Grotesk, sans-serif",
+
+              // colors
+              "@primary-color": "#2f49d1",
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import { Button, Typography } from "antd";
+
+import "./login.less";
+
+const Login = () => {
+  return (
+    <div>
+      <Typography.Paragraph className="para">A para</Typography.Paragraph>
+      <Button type="primary">Hello</Button>
+    </div>
+  );
+};
+
+export default Login;
