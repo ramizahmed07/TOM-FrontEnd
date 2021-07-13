c8ee982b5f47d2e5c7ff3c5e871a28e26abc6740
Rizwan Ahmed <rizwan.ahmed19@outlook.com>
2021-07-13 15:43:56 +0500

    Move less variables in theme.less file

--- craco.config.js
+++ craco.config.js
@@ -17,22 +17,7 @@ module.exports = {
         lessLoaderOptions: {
           lessOptions: {
             modifyVars: {
-              // fonts
-              "@font-family": "Montserrat, sans-serif",
-              "@secondary-font": "HK Grotesk, sans-serif",
-
-              // colors
-              "@primary-color": "#2f49d1",
-              "@primary-color-light": "#8FA6E3",
-              "@secondary-text": "#435465",
-
-              // font sizes
-              "@text-xxl": "40px",
-              "@text-xl": "28px",
-              "@text-lg": "24px",
-              "@text-md": "16px",
-              "@text-sm": "14px",
-              "@text-xs": "12px",
+              hack: `true; @import "./src/styles/theme.less";`,
             },
             javascriptEnabled: true,
           },
