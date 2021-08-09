c4bec0cb090dd6535bd8698ca90fe05a6a2da99b
Ramiz Ahmed <ramizahmedar@gmail.com>
2021-08-10 03:26:41 +0500

    Add sub-industries

--- src/pages/admin/Sectors/index.tsx
+++ src/pages/admin/Sectors/index.tsx
@@ -13,6 +13,7 @@ import {
   useUploadSectorsMutation,
 } from "@services";
 import { IIndustry, ISector } from "@store/sectors";
+import { useTypedSelector } from "@/hooks";
 
 const Sectors = () => {
   const history = useHistory();
